0a3ea29dd2972e11260eafe7240f59ef5e0a2969
jafar <beldarjafar@gmail.com>
2023-08-20 21:15:15 +0530

    added login register

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+
+
+
 // Consts
 const apikey = "7543524441a260664a97044b8e2dc621";
+const youtubeApikey = "AIzaSyDpgOqRhQ6H1Z9jQanld1oLfCsPHZvtGWY";
 const apiEndpoint = "https://api.themoviedb.org/3"
 const imgPath = "https://image.tmdb.org/t/p/original";
-
 
 const apiPaths = {
     fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`,
     fetchMoviesList: (id) => `${apiEndpoint}/discover/movie?api_key=${apikey}&with_genres=${id}`,
     fetchTrending:`${apiEndpoint}/trending/all/day?api_key=${apikey}&language=en-US`,
-    searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC0SZJkHFX-fQ7NrsxdI4l4mGwYuY4l7P8`
+    searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC0SZJkHFX-fQ7NrsxdI4l4mGwYuY4l7P8`,
+    fetchTrailer: (id) =>{},
 }
 
 
